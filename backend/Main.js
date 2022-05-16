63527ef35b693cf5a232cc7b3cb9eb7b44b25d4d
jomoo <jomootgrn88@naver.com>
2022-05-16 22:17:46 +0900

    test

--- backend/Main.js
+++ backend/Main.js
@@ -1,4 +1,3 @@
-//getpath
 var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
@@ -8,28 +7,29 @@ var db_db = require('./db');
 var conn = require('./conn');
 
 app.use(express.json());
+app.use('/home', express.static("./public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.use(bodyParser.urlencoded({extended: false}));
 
 
+app.post('/', (req, res) => {
 
-
-app.post('/', (req,res)=> {
-  
-    conn.user(req,res);
+  conn.user(req, res);
 
 });
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname + 'public/index.html'));
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 
 app.get('/getItem', (req, res) => {
+  console.log("여기까지 왔다.");
   conn.getItem(req, res);
 });
 
-app.listen(3000,function(req,res){
-    console.log('Server started to listen at 3000');
-})
+app.listen(3000, function (req, res) {
+  console.log('Server started to listen at 3000');
+});
