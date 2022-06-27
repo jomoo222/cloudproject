dee0c1d90f4b334cca41376bdeb00a38d24c36c2
jomoo <jomootgrn88@naver.com>
2022-06-27 12:09:29 +0900

    test

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var db = mysql.createConnection({
-	host : '3.38.64.13',  
+	host : '43.200.25.214',  
 	port: '3306',
-	user: 'TT',
-	password : 'passswTT1*',
+	user: 'G',
+	password : 'passswG1*',
 	database : 'CloudContent'
 });
 db.connect();
